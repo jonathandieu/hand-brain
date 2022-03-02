2827433f1dbc1d6632a222faee18a5c16c08879e
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-03-02 15:11:22 -0500

    Now EVERYTHING works

--- cards/server.js
+++ cards/server.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0-4pisv.mongodb.net/COP4331?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+client.connect();
+
 app.use((req, res, next) =>
 {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -125,9 +130,18 @@ app.post('/api/addcard', async (req, res, next) =>
 {
   // incoming: userId, color
   // outgoing: error
-  var error = '';
   const { userId, card } = req.body;
-  // TEMP FOR LOCAL TESTING.
+  const newCard = {Card:card,UserId:userId};
+  var error = '';
+  try
+  {
+    const db = client.db();
+    const result = db.collection('Cards').insertOne(newCard);
+  }
+  catch(e)
+  {
+    error = e.toString();
+  }
   cardList.push( card );
   var ret = { error: error };
   res.status(200).json(ret);
@@ -136,22 +150,21 @@ app.post('/api/login', async (req, res, next) =>
 {
   // incoming: login, password
   // outgoing: id, firstName, lastName, error
-  var error = '';
+ var error = '';
   const { login, password } = req.body;
+  const db = client.db();
+  const results = await
+db.collection('Users').find({Login:login,Password:password}).toArray();
   var id = -1;
   var fn = '';
   var ln = '';
-  if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
+  if( results.length > 0 )
   {
-    id = 1;
-    fn = 'Rick';
-    ln = 'Leinecker';
+    id = results[0].UserId;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
   }
-  else
-  {
-    error = 'Invalid user name/password';
-  }
-  var ret = { id:id, firstName:fn, lastName:ln, error:error};
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
   res.status(200).json(ret);
 });
 app.post('/api/searchcards', async (req, res, next) =>
@@ -160,17 +173,18 @@ app.post('/api/searchcards', async (req, res, next) =>
   // outgoing: results[], error
   var error = '';
   const { userId, search } = req.body;
-  var _search = search.toLowerCase().trim();
+  var _search = search.trim();
+
+  const db = client.db();
+  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+
   var _ret = [];
-  for( var i=0; i<cardList.length; i++ )
+  for( var i=0; i<results.length; i++ )
   {
-    var lowerFromList = cardList[i].toLocaleLowerCase();
-    if( lowerFromList.indexOf( _search ) >= 0 )
-    {
-      _ret.push( cardList[i] );
-    }
+    _ret.push( results[i].Card );
   }
-  var ret = {results:_ret, error:''};
+
+  var ret = {results:_ret, error:error};
   res.status(200).json(ret);
 });
 
